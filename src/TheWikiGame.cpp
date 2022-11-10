03e258bec35d25f32b61df6214fad198abb585de
Kliu2003 <redkevinliu@gmail.com>
2022-11-10 16:01:19 -0600

    Update TheWikiGame.cpp

--- src/TheWikiGame.cpp
+++ src/TheWikiGame.cpp
@@ -1,9 +1,9 @@
 #include "../includes/TheWikiGame.h"
 
-std::vector<std::vector<string>> bfs(std::string startLocation, std::string endLocation) {
+std::vector<std::vector<string>> TheWikiGame::bfs(std::string startLocation, std::string endLocation) {
     return std::vector<std::vector<string>>();
 }
 
-std::vector<std::vector<string>> djikstra(std::string startLocation, std::string endLocation) {
+std::vector<std::vector<string>> TheWikiGame::djikstra(std::string startLocation, std::string endLocation) {
     return std::vector<std::vector<string>>();
 }
