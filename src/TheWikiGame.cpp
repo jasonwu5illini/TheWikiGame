d27e045c37e57bbdb4451f3e2aec8e6f6b325f5a
Kliu2003 <redkevinliu@gmail.com>
2022-11-11 22:05:38 -0600

    Update TheWikiGame.cpp

--- src/TheWikiGame.cpp
+++ src/TheWikiGame.cpp
@@ -6,12 +6,35 @@ std::vector<std::vector<string>> TheWikiGame::bfs(std::string startLocation, std
     std::queue<int>bfSQueue;
     int startId = linkToId[startLocation];
     bfSQueue.push(startId);
+<<<<<<< Updated upstream
     for(int i = 0; i <= 6; i++){
         int degreeSize = bfSQueue.size(); 
+=======
+    int dist = 0;
+    bool foundPath = false;
+    while(dist < 6 && !foundPath){
+        int degreeSize = bfSQueue.size();
+>>>>>>> Stashed changes
         for(int j = 0; j < degreeSize; j++){
-            int adjacentId = degreeSize;
+            int currentPage = bfsQueue.front();
+            bfsQueue.pop();
+            vector<int>linksToOtherPages = directedAdjacencyList[currentPage];
+            for(int k = 0; k < linksToOtherPages.size(); k++){
+                int adjacentId = linksToOtherPages[k];
+                string s = idToLink[adjacentId];
+                if(s == endLocation){
+                    foundPath = true;
+                }
+            }
         }
+<<<<<<< Updated upstream
     } **/
+=======
+        dist++;
+    }
+
+    std::cout<<dist<<std::endl;
+>>>>>>> Stashed changes
     return std::vector<std::vector<string>>();
 }
 
