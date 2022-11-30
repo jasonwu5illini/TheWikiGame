36327c3a020126847978ec1fd17e983e660ff6c2
jasonwu5illini <92773528+jasonwu5illini@users.noreply.github.com>
2022-11-29 22:16:00 -0600

    removed extraneous lines in python script

--- script.py
+++ script.py
@@ -3,8 +3,8 @@ from mediawiki import MediaWiki
 wikipedia = MediaWiki()
 wikipedia.user_agent = 'my-new-user-agent-string'
 
-r = open('rtitles.txt', 'r', encoding='UTF-8') 
-f = open('output.txt', 'a', encoding='UTF-8') 
+#r = open('rtitles.txt', 'r', encoding='UTF-8') 
+#f = open('output.txt', 'a', encoding='UTF-8') 
 with open('rtitles.txt', 'r', encoding='UTF-8') as r:
 	with open('output.txt', 'a', encoding='UTF-8') as f: 
 		for title in r: 
