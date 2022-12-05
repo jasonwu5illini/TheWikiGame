68e90775b540428035918de2cb3c20aa92363a04
Kliu2003 <redkevinliu@gmail.com>
2022-12-04 21:44:14 -0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 int main() {
     //REMEMBER TO CHANGE PATHING EVERYTIME, IT IS DIFFERENT FOR EVERYONE
     //TheWikiGame test = TheWikiGame("/workspaces/cs225/TheWikiGame/ArticleT.txt", "/workspaces/cs225/TheWikiGame/ArticleLinks.txt");
-    TheWikiGame test = TheWikiGame("/workspaces/cs225/TheWikiGame/ArticleTJason.txt", "/workspaces/cs225/TheWikiGame/ArticleLinksJason.txt");
-    std::vector<std::string> path = test.bfs("https://en.wikipedia.org/wiki/Joe_Biden", "https://en.wikipedia.org/wiki/George_Washington");
+    TheWikiGame test = TheWikiGame("/workspaces/CS225 Labs/TheWikiGame/ArticleTKevin.txt", "/workspaces/CS225 Labs/TheWikiGame/ArticleLinksKevin.txt");
+    std::vector<std::string> path = test.bfs("https://en.wikipedia.org/wiki/Nanny_and_Nanko", "https://en.wikipedia.org/wiki/Abraham_Lincoln");
     for(auto it = path.begin(); it != path.end(); it++){
         std::cout<<*it<<std::endl;
     }
